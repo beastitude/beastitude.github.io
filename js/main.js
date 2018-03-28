e2e25d1765238107c36f6d03f35437160ca17bc9
JT <mohnjiles@gmail.com>
2018-03-27 17:52:12 -0700

    added fonts

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(function() {
     $('a[href="#design"]').click(function() {
         $('html, body').animate({
-            scrollTop: $("#design").offset().top - 30
+            scrollTop: $("#design").offset().top - 60
         }, 400);
     });
     $('a[href="#projects"]').click(function() {
         $('html, body').animate({
-            scrollTop: $("#projects").offset().top - 30
+            scrollTop: $("#projects").offset().top - 60
         }, 400);
     });
     $('a[href="#art"]').click(function() {
         $('html, body').animate({
-            scrollTop: $("#art").offset().top - 30
+            scrollTop: $("#art").offset().top - 60
         }, 400);
     });
     $('a[href="#about"]').click(function() {
         $('html, body').animate({
-            scrollTop: $("#about").offset().top - 30
+            scrollTop: $("#about").offset().top - 60
         }, 400);
     });
 });
